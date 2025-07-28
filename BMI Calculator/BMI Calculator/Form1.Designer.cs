42de78677b7511ba557d8d4a98a498618188d40b
GayanHK-tech <gayanhasantha999@gmail.com>
2025-07-28 18:10:51 +0530

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace BMI_Calculator
+{
+    partial class BMIcalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BMIcalculator));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblIndicator = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtBMI = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtIndicator = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCalculate.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(198, 311);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(122, 48);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnReset.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.btnReset.Location = new System.Drawing.Point(407, 311);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnReset.Size = new System.Drawing.Size(108, 48);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.BackColor = System.Drawing.Color.Transparent;
+            this.lblHeight.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(24, 50);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(96, 27);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.BackColor = System.Drawing.Color.Transparent;
+            this.lblWeight.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblWeight.Location = new System.Drawing.Point(24, 130);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(98, 27);
+            this.lblWeight.TabIndex = 3;
+            this.lblWeight.Text = "Weight";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.BackColor = System.Drawing.Color.Transparent;
+            this.lblBMI.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(72, 215);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(48, 24);
+            this.lblBMI.TabIndex = 4;
+            this.lblBMI.Text = "BMI";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStatus.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblStatus.Location = new System.Drawing.Point(402, 52);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(88, 27);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblIndicator
+            // 
+            this.lblIndicator.AutoSize = true;
+            this.lblIndicator.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblIndicator.Font = new System.Drawing.Font("Lucida Calligraphy", 15.75F, System.Drawing.FontStyle.Bold);
+            this.lblIndicator.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblIndicator.Location = new System.Drawing.Point(377, 130);
+            this.lblIndicator.Name = "lblIndicator";
+            this.lblIndicator.Size = new System.Drawing.Size(126, 27);
+            this.lblIndicator.TabIndex = 6;
+            this.lblIndicator.Text = "Indicator";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(126, 52);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(180, 20);
+            this.txtHeight.TabIndex = 7;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(126, 136);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(180, 20);
+            this.txtWeight.TabIndex = 8;
+            // 
+            // txtBMI
+            // 
+            this.txtBMI.Location = new System.Drawing.Point(126, 215);
+            this.txtBMI.Name = "txtBMI";
+            this.txtBMI.Size = new System.Drawing.Size(180, 20);
+            this.txtBMI.TabIndex = 9;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(509, 52);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(180, 20);
+            this.txtStatus.TabIndex = 10;
+            // 
+            // txtIndicator
+            // 
+            this.txtIndicator.Location = new System.Drawing.Point(509, 133);
+            this.txtIndicator.Name = "txtIndicator";
+            this.txtIndicator.Size = new System.Drawing.Size(180, 20);
+            this.txtIndicator.TabIndex = 11;
+            // 
+            // BMIcalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(701, 404);
+            this.Controls.Add(this.txtIndicator);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtBMI);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblIndicator);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "BMIcalculator";
+            this.Text = "Body Mass Index Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblIndicator;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtBMI;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtIndicator;
+    }
+}
+
---
+++ BMI Calculator/BMI Calculator/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class BMIcalculator : Form
+    {
+        public BMIcalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double h, w, BMI;
+            h = Convert.ToDouble(txtHeight.Text);
+            w = Convert.ToDouble(txtWeight.Text);
+            BMI = w / (h * h);
+
+            txtBMI.Text = BMI.ToString();
+
+
+             if (BMI <= 18.5)
+             {
+
+                 txtIndicator.BackColor = Color.Red;
+                 txtStatus.Text = "Under Weight";
+             }
+
+             else if (BMI <= 25)
+             {
+
+                 txtIndicator.BackColor = Color.Blue;
+                 txtStatus.Text = "Normal";
+             }
+
+             else if (BMI <= 30)
+             {
+
+                 txtIndicator.BackColor = Color.Orange;
+                 txtStatus.Text = "Obesity Class 1";
+             }
+
+             else if (BMI <= 40)
+             {
+
+                 txtIndicator.BackColor = Color.Pink;
+                 txtStatus.Text = "Obesity Class 2";
+             }
+
+             else
+             {
+
+                 txtIndicator.BackColor = Color.Brown;
+                 txtStatus.Text = "Obesity Class 3";
+             }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtHeight.Clear();
+            txtWeight.Clear();
+            txtBMI.Clear();
+            txtStatus.Clear();
+            txtIndicator.BackColor = Color.White;
+
+        }
+
+    
+    }
+}
